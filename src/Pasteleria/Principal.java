c17f0d0076c65f0b475c9fd9e008566053fb61b8
mamonreal <ma.monreal@alumnos.urjc.es>
2016-05-03 20:38:30 +0200

    Se ha añadico el método para calcular la complejidad empírica

--- src/Pasteleria/Principal.java
+++ src/Pasteleria/Principal.java
@@ -4,13 +4,20 @@ public class Principal {
     public static void main (String [] args) {
         //Pasteleria pasteleria = new Pasteleria(args[0], args[1]);
         Pasteleria pasteleria = new Pasteleria("entrada.txt", "salida.txt");
+        //Pasteleria pasteleriaRec = new Pasteleria("entrada.txt", "salida.txt");
         //ALGORITMO RECURSIVO
-        //imprimirSolucion(pasteleria.ramificaYPodaRecursivo(pasteleria.getSolucion()));
+        //imprimirSolucion(pasteleriaRec.ramificaYPodaRecursivo(pasteleria.getSolucion()));
         //ALGORITMO ITERATIVO
-        imprimirSolucion(pasteleria.ramificaYPoda());
+        long time_start, time_end;
+        time_start = System.currentTimeMillis();
+        //ReallyHeavyTask(); // llamamos a la tarea
+        Nodo s = pasteleria.ramificaYPoda();
+        time_end = System.currentTimeMillis();
+        System.out.println("the task has taken "+ ( time_end - time_start ) +" milliseconds");
+        imprimirSolucion(s);
         pasteleria.escribirArchivo();
         //Muestra la carpeta predeterminada en la que están los archivos
-        //System.out.println(System.getProperty("user.dir")) ;
+        System.out.println(System.getProperty("user.dir")) ;
     }
     
     public static void imprimirSolucion (Nodo n) {
